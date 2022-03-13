bcc62a71a35e8159a40e9d7c9aa1377bde1b947a
Mausam Khanal <offmausam@gmail.com>
2022-03-13 20:22:39 +0545

    fix icon

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import {
-  Container,
-  Button,
-  Badge,
-} from "react-bootstrap";
+import { Container, Button, Badge } from "react-bootstrap";
 import "./Banner.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
